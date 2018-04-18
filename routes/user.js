4bee21ca4103ee5757a019e3b68186b83d55c994
sdojndx <kofg@live.com>
2018-04-18 03:10:27 -0700

    sample ejs use

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,10 @@ router.get('/',function(req,res){
 })
 router.get('/:name',function(req,res){
 	//console.log("aaa");
-	res.send('hello:'+req.params.name);
+	//res.send('hello:'+req.params.name);
+	res.render('user',{
+		name:req.params.name
+	})
 })
 
 module.exports = router
